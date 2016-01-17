5985893fe4e6fbdac333a09dfbe1eca3b20fb52f
TL <tyler@NASA>
2016-01-17 00:50:57 -0700

    openGL stuff is here

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SOURCES=main.c
 FLAGS=-Wall -pedantic
 LIBDIRS=-L/usr/lib/x86_64-linux-gnu
 INCLUDEDIRS=-I/usr/include
-LIBS=-lSDL2 -lSDL2main
+LIBS=-lSDL2 -lSDL2main -lGL
 OUTFILE=out.a
 
 all:
--- main.c
+++ main.c
@@ -1,5 +1,39 @@
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_opengl.h"
 #include <stdio.h>
+int running = 1;
+
+int initGL()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	if (glGetError() != GL_NO_ERROR)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+void renderGL(SDL_Window* window)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(-0.5f, 0.5f);
+	glEnd();
+	SDL_GL_SwapWindow(window);
+}
+
+void handleEvents()
+{
+
+}
+
 int main(int argc, char** argv)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -14,14 +48,26 @@ int main(int argc, char** argv)
 		SDL_Quit();
 		return -1;
 	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (!renderer)
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	if (!glContext)
 	{
+		printf("OpenGL context failed: %s\n", SDL_GetError());
 		SDL_DestroyWindow(window);
-		printf("SDL_CreateRenderer failed: %s\n", SDL_GetError());
 		SDL_Quit();
 		return -1;
 	}
+	if (!initGL())
+	{
+		printf("OpenGL init failed\n");
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
+	while (running)
+	{
+		renderGL(window);
+		handleEvents();
+	}
+
 	SDL_Quit();
 	return 0;
 }
