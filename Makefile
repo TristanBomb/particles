633ab506ad38f708363a999bde5b0cfec109dc29
TL <tyler@NASA>
2016-01-17 17:35:37 -0700

    it finally compiles with modern GL, requires new libs

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SOURCES=main.c
 FLAGS=-Wall -pedantic
 LIBDIRS=-L/usr/lib/x86_64-linux-gnu
 INCLUDEDIRS=-I/usr/include
-LIBS=-lSDL2 -lSDL2main -lGL
+LIBS=-lSDL2 -lSDL2main -lGL -lGLEW
 OUTFILE=a.out
 
 all:
--- main.c
+++ main.c
@@ -1,31 +1,98 @@
 #include "SDL2/SDL.h"
+#define GLEW_STATIC
+#include "GL/glew.h"
 #include "SDL2/SDL_opengl.h"
 #include <stdio.h>
-int running = 1;
 
-int initGL()
+const char* vertShader =  "#version 150"
+							"in vec2 position //position of the point to shade"
+							"void main()"
+							"{"
+							"	gl_Position = vec4(position, 0.0, 1.0);"
+							"}";
+
+const char* fragShader =   "#version 150"
+							"out vec4 outColor //the color of the output pixel"
+							"void main()"
+							"{"
+							"	outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+							"}";
+
+int running = 1;
+int mouseX = 0; 
+int mouseY = 0;
+
+GLuint makeGLShaders(vertexSource, fragmentSource)
 {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glClearColor(0.f, 0.f, 0.f, 1.f);
-	if (glGetError() != GL_NO_ERROR)
+	//now init the vertex shader
+	GLint status; //gonna use this for both
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, vertexSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	if (status != GL_TRUE)
 	{
 		return 0;
 	}
-	return 1;
+	//now init the fragment (screenspace) shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	if (status != GL_TRUE)
+	{
+		return 0;
+	}
+	//now, put the two into a shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//fragment shaders can write to weird output, but
+	//we want the default: output 0.
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	return shaderProgram;
+}
+
+int initGL()
+{
+	float vertices[] = {
+		-0.5f, 0.5f, //Vertex 1 (x,y)
+		0.5f, 0.5f,  //Vertex 2
+		0.5f, -0.5f, //Vertex 3
+		-0.5f, -0.5f //Vertex 4
+	};
+	GLuint vao; //vertex array object
+	//this just stores calls to "glVertexAttribPointer"
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	GLuint vbo; //vertex buffer object
+	//pretty much, this buffers the vertices to the
+	//GPU instead of keeping them in RAM. this is
+	//also what gets blitted to the screen
+	glGenBuffers(1, &vbo); 
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	GLuint shaderProgram = makeGLShaders(vertShader, fragShader);
+	if (!shaderProgram)
+	{
+		return 0;
+	}
+	glUseProgram(shaderProgram);
+	//now we have to link the input "position" with the actual vertex position
+	//for the vertex shader.
+	GLint posAttribute = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttribute);
+	if (glGetError() == GL_NO_ERROR) {
+		return 1; //woo we did it, we init'd openGL
+	}
+	return 0;
 }
 
 void renderGL(SDL_Window* window)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_QUADS);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.5f, 0.5f);
-		glVertex2f(-0.5f, 0.5f);
-	glEnd();
+	glDrawArrays(GL_TRIANGLES, 0, 4);
 	SDL_GL_SwapWindow(window);
 }
 
@@ -37,6 +104,11 @@ void handleEvents()
 		if (ev.type == SDL_QUIT)
 		{
 			running = 0;
+		} 
+		else if (ev.type == SDL_MOUSEMOTION)
+		{
+			mouseX = ev.motion.x;
+			mouseY = ev.motion.y;
 		}
 	}
 }
@@ -48,6 +120,9 @@ int main(int argc, char** argv)
 		printf("SDL_Init failed: %s\n", SDL_GetError());
 		return -1;
 	}
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 	SDL_Window* window = SDL_CreateWindow("Particles", 0, 0, 1366, 768, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 	if (!window)
 	{
@@ -63,9 +138,17 @@ int main(int argc, char** argv)
 		SDL_Quit();
 		return -1;
 	}
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW init failed: \n");
+		SDL_GL_DeleteContext(glContext);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
 	if (!initGL())
 	{
 		printf("OpenGL init failed\n");
+		SDL_GL_DeleteContext(glContext);
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 	}
@@ -74,7 +157,8 @@ int main(int argc, char** argv)
 		renderGL(window);
 		handleEvents();
 	}
-
+	SDL_GL_DeleteContext(glContext);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
